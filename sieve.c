9790ce5fdb4a8a8599438fe4feae0637d7571beb
Mickey Ju <MickeyJu@gmail.com>
2012-04-22 14:29:01 -0700

    Mickey add is_prime test function.

--- sieve.c
+++ sieve.c
@@ -4,10 +4,12 @@
  */
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>
 
 #define N 10000000
 #define NOT_A_PRIME 0
 #define IS_A_PRIME 1
+#define IS_PRIME(X) is_prime(sieve, N, X)
 
 /*
  * @param sieve an array used to indicate if 2 * index + 3 is a prime
@@ -41,6 +43,37 @@ get_primes(char* sieve, int n) {
 }
 
 int
+is_prime(char* sieve, int n, int num) {
+  if (num < 2) {
+    return NOT_A_PRIME;
+  }
+  if (num == 2) {
+    return IS_A_PRIME;
+  }
+  if ((num & 0x1) == 0) {
+    return NOT_A_PRIME;
+  }
+  if (num <= ((n - 1) << 1) + 3) {
+    return sieve[(num - 3) >> 1];
+  }
+  int sq_root = (int)sqrt((double)num);
+  int prime;
+  int i;
+  for (i = 0; i < n; i++) {
+    prime = i + i + 3;
+    if (prime > sq_root) {
+      break;
+    }
+    if (sieve[i] == IS_A_PRIME) {
+      if (num % prime == 0) {
+        return NOT_A_PRIME;
+      }
+    }
+  }
+  return IS_A_PRIME;
+}
+
+int
 main(int argc, char** argv) {
   char sieve[N];
   get_primes(sieve, N);
@@ -54,6 +87,8 @@ main(int argc, char** argv) {
   }
   /* plus 2, which is not indicated with the array. */
   printf("num of primes: %d\n", count + 1);
+
+  printf("is 2333 a prime number? %d\n", IS_PRIME(2333));
 
   return 0;
 }
